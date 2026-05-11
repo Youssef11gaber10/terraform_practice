50b981da6de790d9440035b417c8904ec639b9a4
youssef gaber <youssef11gaber10@gmail.com>
2026-05-11 05:31:53 +0300

    finish eks cluster provisioning

---
+++ EKS-cluster/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket       = "youssef-terraform-remote-backend"
+    key          = "terraform.tfstate"
+    # key          = "dev/terraform.tfstate"#where you want save state file on bucket 
+    # key          = "test/terraform.tfstate"
+    # key          = "prod/terraform.tfstate"
+
+    region       = "eu-north-1"
+    profile      = "youssef-aws"
+    use_lockfile = true #instead of dyanmodb_table cause use s3 to lock state file 
+  }
+}
